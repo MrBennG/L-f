e8ffeb0f588d33adf4737ffc2a713ef03078d75f
MrBennG <benn@ccpgames.com>
2018-09-27 22:14:41 +0000

    Update Number 2
    RTS style controls v1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject clickedThing = hit.collider.gameObject;
+                print(clickedThing.layer);
+                if (clickedThing.layer != 9)
+                {
+                    if (agent.GetComponent<ClickOn>().currentlySelected == true)
+                    {
+                        agent.SetDestination(hit.point);
+                    }
+                }
+                
+            }
+            
+        }
+    }
+}
